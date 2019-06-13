87ea752fbb186ea6e2d40fe7d60b625022eb8564
BoredKid <cisskane@gmail.com>
2019-06-14 01:51:20 +0200

    add BoredLink component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import BoredLink from './components/BoredLink/BoredLink';
 
+function Stream() {
+  return <h2>Stream</h2>;
+}
+
+function Vod() {
+  return <h2>VOD</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <BoredLink path="/" label="Stream"/>
+            </li>
+            <li>
+              <BoredLink path="/about/" label="About"/>
+            </li>
+            <li>
+              <BoredLink href="https://github.com/BoredKid/BoredStream" label="Repo"/>
+            </li>
+            <li>
+              <BoredLink path="/vod/" label="VOD"/>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Stream} />
+        <Route path="/vod/" component={Vod} />
+        <Route path="/about/" component={About} />
+      </div>
+    </Router>
+  );
+}
 function App() {
   return (
-    <h1>Welcome to the Bored Stream Website ;)</h1>
+    <div>
+      <h1>Welcome to the Bored Stream Website ;)</h1>
+      <AppRouter/>
+    </div>
+
   );
 }
 
---
+++ src/components/BoredLink/BoredLink.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { string } from 'prop-types';
+
+const BoredLink =({path, label, href})=>(
+    path ?
+    <Link to={path}>{label}</Link>:
+    <a href={href}>{label}</a>
+);
+
+BoredLink.propTypes={
+    path: string,
+    label: string,
+    href: string
+}
+
+export default BoredLink;
+
