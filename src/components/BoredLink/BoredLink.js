43c49978969206ba1215d986d6e2790ca74459f5
BoredKid <cisskane@gmail.com>
2019-06-16 00:27:36 +0200

    add style config webpack énorme

--- src/components/BoredLink/BoredLink.js
+++ src/components/BoredLink/BoredLink.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { string } from 'prop-types';
+import './BoredLink.css';
 
-const BoredLink = ({ path, label, href }) => (path ? <Link to={path}>{label}</Link> : <a href={href}>{label}</a>);
+const BoredLink = ({ path, label, href }) => (
+  <div>
+    {path ? (
+      <Link className="BoredLink" to={path}>
+        {label}
+      </Link>
+    ) : (
+      <a className="BoredLink" href={href}>
+        {label}
+      </a>
+    )}
+  </div>
+);
 
 BoredLink.propTypes = {
   path: string,
