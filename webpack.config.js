72232988a0010fcecfe31c607d159b2c38deeb1d
BoredKid <cisskane@gmail.com>
2019-06-23 16:10:31 +0200

    add log for env

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 
 const env = dotenv.config().parsed;
 
+console.log(env);
+
 // reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next]);
